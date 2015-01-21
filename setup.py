af3e484ab05a886da60c41d5949586397983aa8c
Julien Muetton <themouette@gmail.com>
2015-01-21 17:49:16 +0100

    Add test dependencies (compat python >=2.7)

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 
 from setuptools import setup, find_packages
 
+PY3 = sys.version_info[0] == 3
+
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
@@ -18,6 +20,22 @@ requires = [
     # 'git+https://github.com/Gandi/pyramid_kvs.git',
     # 'git+https://github.com/ekini/gsmtpd.git'  # OK I shouldn't, or not ...
     ]
+
+tests_require = ['nose', 'coverage']
+if sys.version_info < (3, 3):
+    tests_require.append('mock')
+
+
+extras_require = {
+    'dev': [
+        'waitress',
+        'pyramid_debugtoolbar',
+    ],
+    'doc': [
+        'sphinx',
+    ],
+    'test': tests_require
+}
 
 setup(name='caliopen.web',
       namespace_packages=['caliopen'],
@@ -38,7 +56,8 @@ setup(name='caliopen.web',
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
-      tests_require=requires,
+      tests_require=tests_require,
+      extras_require=extras_require,
       test_suite="caliopen.web.tests",
       entry_points={
           'paste.app_factory': ['main = caliopen.web:main'],
