617dd914aac27c4e269cf52a0fb5c8ecdb6c2083
Julien Muetton <themouette@gmail.com>
2015-01-28 17:56:26 +0100

    Fixes #13 app route should be restricted to authenticated users

--- caliopen/web/views/app/index.py
+++ caliopen/web/views/app/index.py
@@ -9,14 +9,84 @@ from caliopen.web.views import redirect
 import logging
 log = logging.getLogger(__name__)
 
-@view_config(request_method='GET', renderer='app/index.html',
-             route_name='app.index')
+
+
+class AuthenticationError(Exception):
+    """
+    Used to warn when a user in unauthenticated.
+
+    As authenticated_index core business is shared among several routes
+    and the only difference in implementation is the error handling,
+    This exception is used to tell the view implementation that user is not
+    authenticated, so the view should handle error.
+    """
+    pass
+
+
 def authenticated_index(request):
     """
-    Application Route Index.
+    authenticated_index view business logic.
+    Raises a `AuthenticationError` when user cannot access the view.
+
+    An abstraction is used to allow different error handling depending on
+    request accept header.
+    """
+    if not is_user_authenticated(request):
+        raise AuthenticationError(
+                            'Only authenticated users can access this page')
+
+    # User is authenticated
+    request.response.status_int = 200
+    return { 'user': request.session['user'] }
+
+
+
+@view_config(request_method='GET', renderer='json',
+             route_name='app.index', accept='application/json')
+@view_config(request_method='GET', renderer='json',
+             route_name='app.index', accept='text/json')
+def authenticated_index_api(request):
+    """
+    Application Route Index as JSON.
+
+    Ask authenticated_index to do the job.
+    Returns 401 Unauthorised in case user is unauthenticated.
     """
 
-    return {}
+    try:
+        return authenticated_index(request)
+
+    except AuthenticationError, e:
+        # Unauthorized.
+        # As raising a pyramid.httpexceptions.HTTPUnauthorized does not render
+        # as JSON, format the error manually.
+        request.response.status_int = 401
+        return {
+                'error': e.message
+            }
+
+
+
+@view_config(request_method='GET', renderer='app/index.html',
+             route_name='app.index', accept='text/html')
+def authenticated_index_html(request):
+    """
+    Application Route Index served as HTML.
+
+    Ask authenticated_index to do the job.
+    Redirect to signin in case user is unauthenticated.
+    """
+
+    try:
+        return authenticated_index(request)
+
+    except AuthenticationError, e:
+        # Send the user on signin page.
+        return redirect(request, 'user.signin')
+
+
+
+
 
 @view_config(request_method='GET', route_name='app.public')
 def public_index(request):
