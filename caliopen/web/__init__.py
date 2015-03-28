2acffcc82e815e71c39b61ebd082ae3140a1cce0
Aymeric Barantal <mric@gandi.net>
2015-03-28 12:37:16 +0100

    Ugly hack to init caliopen configuration before pyramid
    Signed-off-by: Aymeric Barantal <mric@gandi.net>

--- caliopen/web/__init__.py
+++ caliopen/web/__init__.py
@@ -1,6 +1,7 @@
 from pyramid.config import Configurator
 
 from .config import includeme  # used by pyramid
+from caliopen.base.config import Configuration
 
 __version__ = '0.0.1'
 
@@ -8,6 +9,9 @@ __version__ = '0.0.1'
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    # XXX ugly way to init caliopen configuration before pyramid
+    caliopen_config = settings['caliopen.config'].split(':')[1]
+    Configuration.load(caliopen_config, 'global')
 
     config = Configurator(settings=settings)
     config.end()
